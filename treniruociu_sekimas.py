f7dc3827002338542a779ebe3aec78cba7d991db
cadainius <142402825+cadainius@users.noreply.github.com>
2023-09-08 10:11:23 +0300

    Create treniruociu_sekimas.py

---
+++ treniruociu_sekimas.py
@@ -0,0 +1,27 @@
+# 1 Dalis Einaras
+# Duomenų lentelės atvaizdavimas: Pagrindinėje programos dalyje turėsime lentelę
+# kuri atvaizduos sportininko treniruočių duomenis. 
+# Lentelė gali būti sudaryta iš stulpelių,
+# kuriuose bus pateikti šie duomenys: data, atlikti pratimai, pakartojimų skaičius, 
+# kilogramai arba kilogramais atlikti pratimai ir pan.
+
+
+# 2 Dalis Lukas
+#Įvedimo, redagavimo ir trynimo funkcionalumas (CRUD):
+#Leiskite naudotojui įvesti naujas treniruotes į lentelę,
+#redaguoti esamas, trinti nepageidaujamus įrašus ir peržiūrėti turimus įrašus. 
+#Tai įgyvendins CRUD funkcionalumą (Create, Read, Update, Delete).
+
+
+# 3 Dalis Eimantas
+#Duomenų saugojimas į failą ir atkūrimas (JSON/Pickle):
+#Sukurkite funkcijas, kurios leis naudotojui išsaugoti treniruočių duomenis į failą, 
+#naudojant JSON arba Pickle formatą. 
+#Be to, reikės funkcijų, kurios atkuria duomenis iš šio failo į programos lentelę.
+
+
+# Dalis Dainius 
+#Duomenų apdorojimo funkcija:
+#Sukurkite funkciją, kuri apdoros sportininko treniruočių duomenis. 
+#Pavyzdžiui, galite šią funkciją panaudoti, kad apskaičiuotumėte, 
+#kokia buvo vidutinė treniruočių trukmė, sumažinote svorius arba kilogramus per tam tikrą laikotarpį ir panašiai. 
